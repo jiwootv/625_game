e78371e005b8069aefb19559981c917e948c8296
Jiwootv <yooziwoo.coding0987@gmail.com>
2024-03-04 18:43:31 +0900

    dasd

---
+++ 6.25 game/data/code/msgbox.py
@@ -0,0 +1,109 @@
+import sys
+
+import pygame
+
+pygame.init()
+DIR = "data\\"
+if __name__ == "__main__": DIR = "..\\"
+
+
+def eventset(r):
+	global EVENT
+	print("EVENT SETTING:", r)
+	EVENT = r
+
+
+class MsgBox:
+	def __init__(self, screen, text, speed, soundtype, size, y=0):
+		self.font = pygame.font.Font(DIR + "font\\DungGeunMo.otf", size)
+		self.y = y
+		self.root = screen
+		self.P = True
+		self.size = size
+		self.text = text
+		self.now_text = ""
+		self.now_text_count = 0
+		self.text_speed = speed
+		self.sound_type = soundtype
+		self.time = 0
+		self.rect1 = pygame.Rect(25, 340, 590, 120)
+		self.rect2 = pygame.Rect(30, 350, 580, 100)
+		self.rect_t = self.font.render(self.text, 1, (255, 255, 255))
+		self.sound = [pygame.mixer.Sound(DIR + "sound\\effect\\A Piano.wav")]
+		self.ev = 0
+		self.draw()
+
+	def t_update(self):
+		self.rect_t = self.font.render(self.now_text, 1, (255, 255, 255))
+		if not len(self.text) == self.now_text_count or self.now_text_count == 0:
+			print("------")
+			self.now_text += self.text[self.now_text_count]
+			self.now_text_count += 1
+			if self.now_text_count != 0: self.sound[self.sound_type].play()
+			print("현재 글자:", self.now_text)
+			print("현재 글자 순서:", self.now_text_count)
+
+	def _draw(self):
+		pygame.draw.rect(self.root, (255, 255, 255), self.rect1)
+		pygame.draw.rect(self.root, (0), self.rect2)
+		self.root.blit(self.rect_t, (15 + self.size, 323 + self.y + self.size))
+
+	def timer(self):
+		self.time = pygame.time.get_ticks()
+
+	def draw(self):
+		clock = pygame.time.Clock()
+		self.P = True
+		while self.P:
+			for event in pygame.event.get():
+				EVENT(event)
+				if event.type == pygame.QUIT:
+					self.P = False
+					clock.tick(30)
+					return "END1"
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_z and len(self.text) == self.now_text_count:
+						self.P = False
+						clock.tick(30)
+						return "END2"
+					if event.key == pygame.K_x:
+						self.now_text = self.text
+						self.now_text_count = len(self.text)
+						print("X")
+			self.t_update()
+			self._draw()
+			pygame.display.update()
+			clock.tick(self.text_speed)
+			pygame.display.update()
+
+
+if __name__ == "__main__":
+
+	running = True
+	clock = pygame.time.Clock()
+	screen = pygame.display.set_mode((640, 480))
+	while True:
+		a = MsgBox(screen, "* 다람쥐가 헌 쳇바퀴를 타고파 하지 않는다..", 10, 0, 25).draw()
+		if a == "END1":
+			pygame.quit()
+			sys.exit()
+		elif a == "END2":
+			print("성공")
+			break
+
+	while True:
+		a = MsgBox(screen, "엄마 꺵째이 지우", 20, 0, 25).draw()
+		if a == "END1":
+			pygame.quit()
+			sys.exit()
+		elif a == "END2":
+			print("성공")
+			break
+
+	while True:
+		screen.fill(0)
+		pygame.display.update()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				print("BER")
+				sys.exit()
---
+++ 6.25 game/data/code/save.py
@@ -0,0 +1,124 @@
+# save code
+
+class Save:
+    def __init__(self):
+        self._file_w = (
+"""Name
+1
+"""
+        )
+        self.file0 = open(r"data\save\file0", "r", encoding="UTF8")
+        self.file1 = open(r"data\save\file1", "r", encoding="UTF8")
+        self.file2 = open(r"data\save\file2", "r", encoding="UTF8")
+        self.file0_r = self.file0.readlines()
+        self.file1_r = self.file1.readlines()
+        self.file2_r = self.file2.readlines()
+        if not self.file0_r:
+            self.file0_r = ["None"]
+
+        if not self.file1:
+            self.file1_r = ["None"]
+
+        if not self.file2:
+            self.file2_r = ["None"]
+        print(self.file0_r)
+        self.file0.close()
+        self.file1.close()
+        self.file2.close()
+
+        self.file0 = open(r"data\save\file0", "w", encoding="UTF8")
+        self.file1 = open(r"data\save\file1", "w", encoding="UTF8")
+        self.file2 = open(r"data\save\file2", "w", encoding="UTF8")
+        self.ROOMLIST = ["없음", "감옥", "감옥 복도", "감옥 복도"]
+        # load file
+        self.Pname = ""
+        self.P_room = 0
+        self.P_gun = 0
+
+    def __roomlist(self):
+        return self.ROOMLIST
+
+    def edit(self, type, r, filenum):
+        if filenum == 0:
+            if type == "name":
+                if len(self.file0_r) == 0: self.file0_r.append(r)
+                else: self.file0_r[0] = r
+        if filenum == 1:
+            if type == "name":
+                if len(self.file0_r) == 0: self.file0_r.append(r)
+                else: self.file1_r[1] = r
+        if filenum == 2:
+            if type == "name":
+                if len(self.file0_r) == 0: self.file0_r.append(r)
+                else: self.file2_r[2] = r
+
+
+    def write(self):
+        print("File")
+        for i in self.file0_r:
+            self.file0.write(i)
+        for i in self.file1_r:
+            self.file1.write(i)
+        for i in self.file2_r:
+            self.file2.write(i)
+        self.file0.close()
+        self.file1.close()
+        self.file2.close()
+
+    def is_none(self, file_number):
+        return eval('len(self.file{}_r)==0'.format(file_number, file_number))
+
+    def load(self, type, filenum):
+        return eval(f"self.file{filenum}_r[{type}]")
+
+
+class Save_Ini:
+    def __init__(self):
+        from configparser import ConfigParser
+
+        self.config = ConfigParser()
+
+        ret = self.config.read(r'data\save\t.ini')
+
+        found = False
+        if not ret:
+            print("INI 파일이 존재하지 않음")
+        else:
+            found = True
+            print("INI 파일 존재")
+
+        # INI 파일이 존재하지 않으면 INI 파일을 생성합니다.
+        if not found:
+            self.config.add_section('Setting')
+            self.config.set('Setting', '_SE_Volume', '1.00')
+            self.config.set('Setting', '_MUS_Volume', '1.00')
+            self.config.set('Setting', 'Difficult', 'True')
+
+            self.config.add_section("Achievement")
+            self.config.set("Achievement", "_CLEAR_NUMBER", "0")
+            self.config.set("Achievement", "_M_ISTEREGG", "0")
+
+            with open(r'data\save\t.ini', 'w') as configfile:
+                configfile.write("# 누군가 이 1950 : 정지 파일을 보고 있네요\n# 같은 게임 뜯기 좋아하는 사람으로써 너무 고맙습니다..\n")
+                self.config.write(configfile)
+
+            print('INI 파일 생성')
+
+        self._SE_Volume = self.config.get("Setting", "_SE_Volume")
+        self._MUS_Volume = self.config.get("Setting", "_MUS_Volume")
+        self.Difficult = self.config.get("Setting", "Difficult")
+        self.E_M = self.config.get("Achievement", "_m_isteregg")
+        print(self.E_M)
+
+    def get_ini(self, t):
+        a = eval(f"self.{t}")
+        return a
+
+    def edit_ini(self, s, o, r):
+        self.config.set(s, o, r)
+        with open(r'data\save\t.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+# print(Save_Ini().get_ini("_SE_Volume"))
+
+# INI 파일이 존재하면 읽어와서 출력하고 일부 값을 변경해서 다시 저장합니다.
